4c1e5b61afbef761cceef0a83e4560967f90ae2a
Vyacheslav Yeldyshov <slavae222@gmail.com>
2024-04-28 17:26:50 +0500

    basic structure & app routs & styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+import { TicketComponent } from './pages/ticket/ticket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'tickets',
+    component: TicketsComponent,
+  },
+  {
+    path: 'tickets/:id',
+    component: TicketComponent,
+  },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/shared/layouts/layout-full-screen/layout-full-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-full-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-full-screen.component.html',
+  styleUrl: './layout-full-scrin.component.scss',
+})
+export class LayoutFullScreenComponent {}
